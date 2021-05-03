cd9cbad1c783e05dd23bf07ff82e182a90476888
Alperen Mutlu <chetto_17@hotmail.com>
2021-05-03 22:51:44 +0300

    Add project files.

---
+++ DataAccess/Abstract/IProductDal.cs
@@ -0,0 +1,13 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    //product ile ilgili veri tabanında yapılacak operasyonları içerir.
+    public interface IProductDal : IEntityRepository<Product>
+    {
+       
+    }
+}
