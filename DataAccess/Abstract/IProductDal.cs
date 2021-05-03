e51eb77c4022fdde5fd68f6046b22e05a299153b
Alperen Mutlu <chetto_17@hotmail.com>
2021-05-04 02:48:04 +0300

    ProductDetailDto eklendi.

--- DataAccess/Abstract/IProductDal.cs
+++ DataAccess/Abstract/IProductDal.cs
@@ -1,5 +1,6 @@
 ﻿using Core.DataAccess;
 using Entities.Concrete;
+using Entities.DTOs;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -9,6 +10,6 @@ namespace DataAccess.Abstract
     //product ile ilgili veri tabanında yapılacak operasyonları içerir.
     public interface IProductDal : IEntityRepository<Product>
     {
-
+        List<ProductDetailDto> GetProductDetails();
     }
 }
