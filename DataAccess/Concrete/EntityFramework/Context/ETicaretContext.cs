2e7f34baaa50620bd45daece645ec763ac2cde18
Alperen Mutlu <chetto_17@hotmail.com>
2021-05-04 01:57:03 +0300

    Core katmanı eklendi.

--- DataAccess/Concrete/EntityFramework/Context/ETicaretContext.cs
+++ DataAccess/Concrete/EntityFramework/Context/ETicaretContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -12,6 +13,8 @@ namespace DataAccess.Concrete.EntityFramework.Context
             optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=ETicaret;Trusted_Connection=true");
         }
 
-        prop
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Customer> Customers { get; set; }
     }
 }
